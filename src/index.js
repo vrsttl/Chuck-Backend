1743e5a8352847dd609e734fd03dc8c568205594
Attila Veres <vrsttl@gmail.com>
2018-10-10 13:40:12 +0200

    refact(master)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.SERVPORT;
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
